0c11038e030e650be9e97f7f7d627ff94da0c636
Samuel <49483389+SamuelFX@users.noreply.github.com>
2020-05-26 19:16:21 -0400

    Create Storage.h

---
+++ Storage.h
@@ -0,0 +1,21 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+/* Text Database */
+
+vector<string> TextDB = { "*2~", "*.sW" };
+
+const string Ta1 = "NanoCore Client";
+const string Ta2 = "get_MachineName";
+
+/* End Text Database */
+
+/* Hex Database*/
+
+const string Ha1 = "50414444494e475858"; // PADDINGXX
+
+/* End Hex Database*/
+
+int Score;
